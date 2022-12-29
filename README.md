2fdae26d2f964272337133705919ceaa696b8a6a
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-12-29 21:10:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Wiki
+
+FOME Wiki built using [Docusaurus 2](https://docusaurus.io/).
+
+## Installation
+
+Required [Node.js](https://nodejs.dev/) version: 16 or higher.
+
+```bash
+npm install
+```
+
+## Local Development
+
+```bash
+npm start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+## Build
+
+```bash
+npm build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+## Generate PDF
+
+Build and serve docs locally before generating PDF:
+
+```bash
+npm run build
+npm run serve
+```
+
+In another terminal run this to generate PDF:
+
+```bash
+npm run pdf
+```
---
+++ src/pages/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import pdfUrl from '@site/static/pdf/docs.pdf';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div style={{ marginBottom: 10 }}>
+          <div className={styles.buttons}>
+            <a
+              href={pdfUrl}
+              className="button button--secondary button--lg"
+            >
+              📗 PDF version here
+            </a>
+          </div>
+        </div>
+        <div>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro"
+            >
+              🌎 Online version here
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
